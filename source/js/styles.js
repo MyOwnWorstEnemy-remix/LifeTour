de5b10cee65055bb59d3a6c8fced4df58abcb3a1
HurricaneLily <jostin27022023@gmail.com>
2024-08-26 11:53:33 +0400

    Styles

--- source/js/styles.js
+++ source/js/styles.js
@@ -1,10 +1,6 @@
 const process = document.querySelector('.training__process');
 const steps = process.querySelectorAll('.training__step');
 
-const height = steps[1].offsetHeight + steps[2].offsetHeight + 25*3 + steps[0].offsetHeight;
-for (let i = 0; i < steps.length; i++) {
-  console.log(steps[i].offsetHeight);
-  console.log(height)
-}
+const height = steps[1].offsetHeight + steps[2].offsetHeight + 25 * 3 + steps[0].offsetHeight;
 process.style.setProperty('--line-height', `${height}px`);
-process.style.setProperty('--top', `${steps[0].offsetHeight/2}px`)
+process.style.setProperty('--top', `${steps[0].offsetHeight / 2}px`);
